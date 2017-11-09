57ee2ac5258b9ca40135f0a88dd3fc909f167670
vagrant <vagrant@homestead>
2017-11-09 11:13:17 +0000

    delete the existing file for new upload image

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,17 +23,17 @@ class UsersController extends Controller
     public function update(Request $request, $user)
     {
 		$auth = User::findOrFail($user);
-		$path = request()->file('image')->store('upload_image');
-
-		$this->validate(request(), [
+ 		$this->validate(request(), [
 			 'email' =>  'unique:users,email,'. $auth->id,
 			 'password' => 'nullable|min:8',
 			 'image' => 'mimes:jpeg,jpg,png|dimensions:min_width=100,min_height=200',
 		]);
 
+		Storage::disk('local')->delete($auth->image_path);
+		$auth->image_path = request()->file('image')->store('upload_image'); 
+
 		$auth->name = request('name');
 		$auth->email = request('email');
-		$auth->image_path = $path;
 		$auth->password = bcrypt(request('password'));
 		$auth->save();
 
